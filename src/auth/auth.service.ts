9e448c1bfe27362a864764882e4c924a5e82c077
David <davidacumora@gmail.com>
2024-06-15 07:58:49 -0600

    Delete validation user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
     pass: string,
   ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOneByMail(mail);
-    const volunteer = await this.volunteerService.findOne(5);
+    
     
 
     if (user?.password !== pass) {
@@ -27,23 +27,7 @@ export class AuthService {
     
     
 
-    if (volunteer) {
-      if (!volunteer.itsVerified) {
-        throw new HttpException('Su cuenta aun no ha sido verificada!', HttpStatus.METHOD_NOT_ALLOWED);
-      }
-    } else {
-      // Intentar encontrar el usuario como organization
-      const organization = await this.organizationService.findOne(user.id);
-  
-      if (organization) {
-        if (!organization.verified) {
-          throw new HttpException('La cuenta de la organización aun no ha sido verificada!', HttpStatus.METHOD_NOT_ALLOWED);
-        }
-      } else {
-        // Si el usuario no es ni volunteer ni organization
-        throw new UnauthorizedException('Usuario no válido');
-      }
-    }
+    
     const payload = { sub: user.id, username: user.mail };
     return {
       access_token: await this.jwtService.signAsync(payload),
